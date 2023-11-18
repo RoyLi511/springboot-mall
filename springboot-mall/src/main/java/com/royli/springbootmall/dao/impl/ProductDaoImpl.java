e49c7d2d3ffddaa0835962d19a79cf4f4f5d9999
LAPTOP-B0JU64E2\Roy <rrr111999o5@gmail.com>
2023-11-18 19:28:55 +0800

    「商品功能 - 實作查詢商品列表 (分頁 + 總筆數)」

--- springboot-mall/src/main/java/com/royli/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/royli/springbootmall/dao/impl/ProductDaoImpl.java
@@ -24,6 +24,28 @@ public class ProductDaoImpl implements ProductDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
+    public Integer countProduct(ProductQueryParams productQueryParams) {
+        String sql = "SELECT count(*) FROM product WHERE 1=1";
+
+        Map<String, Object> map = new HashMap<>();
+
+        // 查詢條件
+        if (productQueryParams.getCategory() != null) {
+            sql = sql + " AND category = :category";
+            map.put("category", productQueryParams.getCategory().name());
+        }
+
+        if (productQueryParams.getSearch() != null) {
+            sql = sql + " AND product_name LIKE :search"; // %:search% 不可以這樣寫
+            map.put("search", "%" + productQueryParams.getSearch() + "%");  // 模糊查詢
+        }
+
+        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+
+        return total;
+    }
+
+    @Override
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
         String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, " +
                 "created_date, last_modified_date " +
