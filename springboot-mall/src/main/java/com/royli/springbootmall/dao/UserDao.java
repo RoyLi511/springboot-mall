145e9147d79ebf17d99c0e7e7287ca8b2977ae52
LAPTOP-B0JU64E2\Roy <rrr111999o5@gmail.com>
2023-11-20 13:46:46 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/royli/springbootmall/dao/UserDao.java
+++ springboot-mall/src/main/java/com/royli/springbootmall/dao/UserDao.java
@@ -6,6 +6,7 @@ import com.royli.springbootmall.model.User;
 public interface UserDao {
 
     User getUserById(Integer userId);
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 
 }
